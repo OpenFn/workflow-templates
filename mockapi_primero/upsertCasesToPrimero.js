cb6ead359a900106e09e98caab28199ecf3e19ef
ritazagoni <zagoni.rita@gmail.com>
2023-01-24 12:04:10 +0300

    mock commcareapi to primero jobs

---
+++ mockapi_primero/upsertCasesToPrimero.js
@@ -0,0 +1,41 @@
+alterState(state => {
+  let people = [];
+  if (Array.isArray(state.data)) {
+    people = Array.from(state.data);
+  } else {
+    people.push(state.data);
+  }
+    console.log(people);
+
+  return {
+    ...state,
+    people,
+  };
+});
+  
+each(
+    '$.people[*]', 
+    alterState(state => {
+      const primeroCase = state.data;
+
+      let data = {
+        nationality: [primeroCase.nationality],
+        ethnicity: [primeroCase.ethnicity],
+        record_id: primeroCase.case_id,
+        
+      };
+      const boys_and_girls =  {
+          "boys_333245": primeroCase.boys,
+          "girls_895184": primeroCase.girls,
+          "total": primeroCase.total
+      };
+    data["total_number_of_boys_and_girls_in_family_e6c9373"]  = boys_and_girls;
+    console.log('Upserting case', JSON.stringify(data, null, 2));
+    return upsertCase({
+      externalIds: ['record_id'],
+      data,
+    },
+    state => {
+      return state;
+    })(state)
+}));
