527c01c5504a1def0dc733825fa7387e5b534aef
Rita Zágoni <zagoni.rita@gmail.com>
2023-05-12 13:43:59 +0200

    Create upsertToDhis2.js

---
+++ kobo-dhis2/upsertToDhis2.js
@@ -0,0 +1,39 @@
+fn(state => {
+  console.log(state.response.data.results);
+  const submissions = state.response.data.results;
+  const submission = submissions.slice(0)[0];
+  return { ...state, submission };
+});
+
+if (state.submission) {
+//Load datavalue sets using DHIS2's version of "UPSERT", using "importStrategy: 'CREATE_AND_UPDATE' by default`)
+  create("dataValueSets", {
+    dataSet: "QX4ZTUbOt3a", //Reproductive health
+    completeDate: new Date().toISOString().split('T')[0], //Today's date in YYYY-MM-DD format
+    period: "202303", //Mar 2023
+    orgUnit: "ImspTQPwCqd", //Sierra Leone
+    dataValues: [
+      {
+        dataElement: "fbfJHSPpUQD", //ANC 1st Visit
+        // value: 7
+        value: state => parseInt(state.submission.ANC1),
+        
+        categoryOptionCombo: "pq2XI5kz2BY" // Fixed
+        },
+      {
+        dataElement: "cYeuwXTCPkU", //ANC 2nd Visit
+        value: state => parseInt(state.submission.ANC_2nd_Visit),
+        categoryOptionCombo: "pq2XI5kz2BY"
+
+      },
+      {
+        dataElement: "Jtf34kNZhzP", //ANC 3rd Visit
+        value: state => parseInt(state.submission.ANC_3rd_Visit),
+        categoryOptionCombo: "pq2XI5kz2BY"
+
+      }
+    ],
+  })
+} else {
+  console.log("No new submissions were found.")
+};
